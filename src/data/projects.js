bce38db3b2d7f3160009af6f1e4c01f15047a360
learnwitharryan-code <learnwitharryan@gmail.com>
2026-05-07 09:17:57 +0530

    Auto-sync: update showcase videos

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,33 +6,24 @@ export const projects = [
     "thumbnail": "https://images.unsplash.com/photo-1618761714954-0b8cd0026356?auto=format&fit=crop&q=80&w=1000",
     "videoUrl": "/cgi-work.mp4",
     "span": "",
-    "progress": 70
+    "progress": 77
   },
   {
     "id": 2,
-    "title": "Main",
-    "category": "Cinematic Showcase",
-    "thumbnail": "https://images.unsplash.com/photo-1618761714954-0b8cd0026356?auto=format&fit=crop&q=80&w=1000",
-    "videoUrl": "/main.mp4",
-    "span": "",
-    "progress": 64
-  },
-  {
-    "id": 3,
     "title": "Ui Animation 2",
     "category": "Cinematic Showcase",
     "thumbnail": "https://images.unsplash.com/photo-1618761714954-0b8cd0026356?auto=format&fit=crop&q=80&w=1000",
     "videoUrl": "/ui-animation-2.mp4",
     "span": "",
-    "progress": 47
+    "progress": 56
   },
   {
-    "id": 4,
+    "id": 3,
     "title": "Ui Animation",
     "category": "Cinematic Showcase",
     "thumbnail": "https://images.unsplash.com/photo-1618761714954-0b8cd0026356?auto=format&fit=crop&q=80&w=1000",
     "videoUrl": "/ui-animation.mp4",
     "span": "",
-    "progress": 60
+    "progress": 33
   }
 ];
